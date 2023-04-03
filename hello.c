f5b8c822a9381e4a9bb4782808c466e460ddd737
Sabbir Hossen <sabbir.ms2000@gmail.com>
2023-04-03 12:07:42 +0600

    save

--- hello.c
+++ hello.c
@@ -1,6 +1,31 @@
+// #include <stdio.h>
+// int main()
+// {
+//     printf("hello bangladesh\n");
+//     printf("I want to be a\tsoftware engineer\n");
+//     printf("Do you stay with me ! ");
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main()
+// {
+//     int sabbir = 10;
+//     float sakib = 1.5;
+//     char sam = '!';
+//     printf("%d,%0.1f,%c", sabbir, sakib, sam);
+//     return 0;
+// }
+
+// another system
+
 #include <stdio.h>
 int main()
 {
-    printf("hello bangladesh yess no");
+    int rahim;
+    float f;
+    char c;
+    scanf("%d %f %c", &rahim, &f, &c);
+    printf("%d %0.2f %c", rahim, f, c);
     return 0;
 }
