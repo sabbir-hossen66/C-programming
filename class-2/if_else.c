081486ddd65433922f1642e616d26de661069ed3
sabbir-hossen66 <sabbir.ms2000@gmail.com>
2025-10-10 16:18:55 +0600

    condition operator

---
+++ class-2/if_else.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+  
+     float tk;
+     scanf("%f",&tk);
+     if(tk >= 50.50)
+     {
+        printf("khabo khabo");
+     }
+     else{
+        printf("khabo na");
+     }
+
+    return 0;
+}
