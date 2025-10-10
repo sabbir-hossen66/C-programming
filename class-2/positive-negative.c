42272b4951022ff612e826c83abfec2f105dfc4e
sabbir-hossen66 <sabbir.ms2000@gmail.com>
2025-10-10 17:29:13 +0600

    problem 2 slove

---
+++ class-2/positive-negative.c
@@ -0,0 +1,16 @@
+//Take a number from user and check if its a positive or negative number.
+#include<stdio.h>
+int main(){
+   int num;
+   scanf("%d",&num);
+   if(num > 0){
+    printf("%d is a positive number",num);
+   }
+      else if(num < 0){
+       printf("%d is a negative number", num);
+   }
+   else{
+    printf("%d is a negative",num);
+   }
+    return 0;
+}
