3a43d346bb84b3d351c70c06e9a7a5b6db23092f
sabbir-hossen66 <sabbir.ms2000@gmail.com>
2025-10-14 22:34:34 +0600

    condition check

---
+++ class-2/float-problem.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+
+    // Read the input value N
+    scanf("%d", &N);
+
+  
+    for (int i = 1; i <= N; i++) {
+        
+
+        if (i % 3 == 0 && i%7==0) {
+         
+            printf("%d\n", i);
+        }
+    }
+
+    return 0;
+}
