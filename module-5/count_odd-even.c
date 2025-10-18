1baf070bf48bb2e57e9fd644df4e89f7f572b9fd
sabbir-hossen66 <sabbir.ms2000@gmail.com>
2025-10-19 00:57:13 +0600

    total count

---
+++ module-5/count_odd-even.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+    int n ,a;
+    int even=0,odd=0,pos=0,neg=0;
+    scanf("%d",&n);
+    for (int i = 0; i < n; i++)
+    { 
+        scanf("%d",&a);
+        if(a%2==0)
+        even++;
+         else
+         odd++;
+         if(a>0)        
+         pos++;
+         else if(a<0)
+         neg++;
+    }
+    printf("Even: %d\n",even);
+    printf("Odd: %d\n",odd);
+    printf("Positive: %d\n",pos);
+    printf("Negative: %d \n",neg);
+    return 0;
+}
+
