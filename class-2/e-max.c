da540317b1cf534cdc661a9d7d8a076a5e2dccf5
sabbir-hossen66 <sabbir.ms2000@gmail.com>
2025-10-20 23:43:10 +0600

    fix multiplication

--- class-2/e-max.c
+++ class-2/e-max.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
+#include<limits.h>
 
 int main(){
   int n;
   scanf("%d",&n);
-  int max=0;
+  int max=INT_MIN;
 
   for(int i=1;i<=n;i++){
    int val;
    scanf("%d", &val);
-   if(max<val){
+   if(max < val){
    max=val;
 }
 
