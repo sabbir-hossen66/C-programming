799bd6ea3e0c71c37ca2f8e0c2f693c2b362ad73
sabbir-hossen66 <sabbir.ms2000@gmail.com>
2025-10-23 23:54:21 +0600

    test

---
+++ problem-solving-5/consecutive-odd-number-sum.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+  int n;
+  scanf("%d", &n);
+  int x,y;
+    int count = 0;
+  for (int i = 0; i < count; i++)
+  {
+        if (n % 2 != 0)
+        {
+            printf("%d\n", n);
+            break;
+        }
+        n++;
+  }
+  
+    return 0;
+}
